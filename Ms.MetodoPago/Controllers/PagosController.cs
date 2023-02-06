21c96b07a6ff8e4ee96868b8665ad0cde723dcb8
warojas <99766376+warojas@users.noreply.github.com>
2023-02-05 20:42:29 -0500

    controladorPagos

--- Ms.MetodoPago/Controllers/PagosController.cs
+++ Ms.MetodoPago/Controllers/PagosController.cs
@@ -1,6 +1,41 @@
-﻿namespace Ms.MetodoPago.API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ms.MetodoPago.API.Controllers
 {
+    [ApiController]
     public class PagosController
     {
+        [HttpGet(ApiRoutes.RouteHabitacion.GetAll)]
+        public IEnumerable<dominio.Habitacion> ListarHabitaciones()
+        {
+            HabitacionQueryGetAll objProducto = new HabitacionQueryGetAll();
+            var listaHabitacion = objProducto.ListarHabitacion();
+
+            return listaHabitacion;
+        }
+
+
+        [HttpPost(ApiRoutes.RouteHabitacion.Create)]
+        public ActionResult<dominio.Habitacion> CrearHabitacion(dominio.Habitacion habitacion)
+        {
+            HabitacionCommandCreate objHabitacion = new HabitacionCommandCreate();
+            return objHabitacion.CrearHabitacion(habitacion);
+        }
+
+
+        [HttpPut(ApiRoutes.RouteHabitacion.Update)]
+        public ActionResult<dominio.Habitacion> ModificarProducto(dominio.Habitacion habitacion)
+        {
+            HabitacionCommandUpdate objHabitacion = new HabitacionCommandUpdate();
+            return objHabitacion.ModificarHabitacion(habitacion);
+        }
+
+
+        [HttpDelete(ApiRoutes.RouteHabitacion.Delete)]
+        public ActionResult<dominio.Habitacion> EliminarProducto(string id)
+        {
+            HabitacionCommandDelete objHabitacion = new HabitacionCommandDelete();
+            return objHabitacion.EliminarProducto(id);
+        }
     }
 }
---
+++ Ms.MetodoPago/Routes/ApiRoutes.cs
@@ -0,0 +1,21 @@
+﻿namespace Ms.MetodoPago.API.Routes
+{
+    public class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class RouteHabitacion
+        {
+            //Read
+            public const string GetAll = Base + "/pagos/all";
+            public const string GetById = Base + "/pagos/{id}";
+
+            //Write
+            public const string Create = Base + "/pagos/create";
+            public const string Update = Base + "/pagos/update";
+            public const string Delete = Base + "/pagos/delete";
+        }
+    }
+}
