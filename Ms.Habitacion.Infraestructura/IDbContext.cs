a13672b75573b88df3abad1e85817fae7c316ae8
valentin99san <valentinlopez.sandro@gmail.com>
2023-02-06 10:52:03 -0500

    Ms Hbaitacion, Agrega BbContext en la capa infrestructura

---
+++ Ms.Habitacion.Infraestructura/IDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Release.MongoDB.Repository;
+
+namespace Ms.Habitacion.Infraestructura
+{
+    public interface IDbContext : IDataContext
+    {
+
+    }
+}
