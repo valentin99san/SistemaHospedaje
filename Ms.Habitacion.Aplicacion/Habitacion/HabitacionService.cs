323810d5f54f660b7353e12edff5d7b4665b8ad0
valentin99san <valentinlopez.sandro@gmail.com>
2023-02-06 11:09:03 -0500

    Ms.Habitacion, se implemeto HabitacionService e IHabitacionService en la capa Aplicacion

---
+++ Ms.Habitacion.Aplicacion/Habitacion/HabitacionService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using dominio = Ms.Habitacion.Dominio.Entidades;
+using Release.MongoDB.Repository;
+
+namespace Ms.Habitacion.Aplicacion.Habitacion
+{
+    public class HabitacionService : IHabitacionService
+    {
+        private readonly ICollectionContext<dominio.Habitacion> _habitacion;
+        private readonly IBaseRepository<dominio.Habitacion> _habitacionR;
+
+        public HabitacionService(ICollectionContext<dominio.Habitacion> habitacion,
+                                IBaseRepository<dominio.Habitacion> habitacionR)
+        {
+            _habitacion = habitacion;
+            _habitacionR = habitacionR;
+        }
+
+        public List<dominio.Habitacion> ListarHabitaciones()
+        {
+            Expression<Func<dominio.Habitacion, bool>> filter = s => s.esEliminado == false;
+            var items = (_habitacion.Context().FindAsync(filter, null).Result).ToList();
+            return items;
+        }
+
+        public bool Registrar(dominio.Habitacion producto)
+        {
+            producto.esEliminado = false;
+            producto.fechaCreacion = DateTime.Now;
+            producto.esActivo = true;
+
+            // _producto.Context().InsertOne(producto);                       
+
+            var p = _habitacionR.InsertOne(producto);
+
+            return true;
+        }
+
+        public dominio.Habitacion BuscarPorId(int idhabitacion)
+        {
+            Expression<Func<dominio.Habitacion, bool>> filter = s => s.esEliminado == false && s.id_habit == idhabitacion;
+            var item = (_habitacion.Context().FindAsync(filter, null).Result).FirstOrDefault();
+            return item;
+        }
+
+        public void Eliminar(int idHabitacion)
+        {
+            Expression<Func<dominio.Habitacion, bool>> filter = s => s.esEliminado == false && s.id_habit == idHabitacion;
+            var item = (_habitacion.Context().FindOneAndDelete(filter, null));
+
+        }
+
+        public bool Modificar(dominio.Habitacion producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ActualizarStock(int idHabitacion)
+        {
+            var habitacion = BuscarPorId(idHabitacion);
+
+            _habitacionR.UpdateOne(habitacion.id, habitacion);
+
+            return true;
+        }
+
+     
+    }
+}
---
+++ Ms.Habitacion.Aplicacion/Habitacion/IHabitacionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio = Ms.Habitacion.Dominio.Entidades;
+
+namespace Ms.Habitacion.Aplicacion.Habitacion
+{
+    public interface IHabitacionService
+    {
+        // Read
+        List<dominio.Habitacion> ListarHabitaciones();
+        dominio.Habitacion BuscarPorId(int idHabitacion);
+
+        // Write
+        bool Registrar(dominio.Habitacion habitacion);
+        bool Modificar(dominio.Habitacion habitacion);
+        void Eliminar(int idHabitacion);
+        bool ActualizarStock(int idHabitacion);
+    }
+}
