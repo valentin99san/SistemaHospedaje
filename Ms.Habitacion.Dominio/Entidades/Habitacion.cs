98ed1bc1c8dfb46c0733283017d04a08de45478d
valentin99san <valentinlopez.sandro@gmail.com>
2023-02-06 11:01:40 -0500

    Ms Habitacion, Se modifico la entidad Habitacion en la capa dominio

--- Ms.Habitacion.Dominio/Entidades/Habitacion.cs
+++ Ms.Habitacion.Dominio/Entidades/Habitacion.cs
@@ -5,15 +5,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Release.MongoDB.Repository;
+using Release.MongoDB.Repository.Model;
 
 namespace Ms.Habitacion.Dominio.Entidades
 {
-    public class Habitacion
+    [CollectionProperty("Habitacion")]
+    [BsonIgnoreExtraElements]
+    public class Habitacion : EntityToLower<ObjectId>
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string _id { get; set; }
-
         public int id_habit { get; set; }
 
         public int num_habit { get; set; }
@@ -21,7 +21,7 @@ namespace Ms.Habitacion.Dominio.Entidades
         public int num_piso { get; set; }
 
         public string estado_habit { get; set; }
-        
+
         public string tipo_habit { get; set; }
     }
 }
